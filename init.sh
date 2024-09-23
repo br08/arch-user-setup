e9ae39370087e8d305ae87ac106042c78df633ed
Brayan Mendonça <brayan.neves@gmail.com>
2024-09-23 13:50:25 -0300

    fix: fix ssh-agent not starting after login

--- init.sh
+++ init.sh
@@ -16,19 +16,26 @@ passwd $username
 sed -i 's/^#\s*\(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
 sed -i "/^\[user\]/,/^$/ {/^default=/d}; \$a [user]\ndefault=${username}" /etc/wsl.conf
 
+usershell=$(getent passwd $username | cut -d: -f7)
+shellname=$(basename $usershell)
+shellrc="/home/$username/.$shellname"rc
+
+cat << 'EOF' >> "$shellrc"
+
+if [ -z "$SSH_AUTH_SOCK" ]; then
+  eval "$(ssh-agent -s)" > /dev/null
+  ssh-add ~/.ssh/id_ed25519 2> /dev/null
+fi
+
+cd ~/
+EOF
+
 printf "\n\rGenerating new ssh key..."
 printf "\n\rSet your email:"
 read -p " " useremail < /dev/tty
 [ ! -d /home/$username/.ssh ] && mkdir /home/$username/.ssh
 ssh-keygen -t ed25519 -f /home/$username/.ssh/id_ed25519 -C "$useremail"
 
-echo "
-if [ -z \"\$SSH_AUTH_SOCK\" ]; then
-  eval \"\$(ssh-agent -s)\" > /dev/null
-  ssh-add ~/.ssh/id_ed25519 2> /dev/null
-fi
-
-cd ~/
-" >> "/home/$username/.$(echo $0 | sed 's/^-//')rc"
+chown -R $username:$username /home/$username/.ssh
 
 su - $username
