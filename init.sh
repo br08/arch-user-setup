cfcb94e1448ce50aa3435bf619f1c348c7ae7341
Brayan Mendonça <brayan.neves@gmail.com>
2024-09-25 17:05:00 -0300

    refactor: change it to be posix compliant

--- init.sh
+++ init.sh
@@ -7,11 +7,11 @@ pacman-key --init
 pacman-key --populate archlinux
 pacman -Syu --noconfirm openssh git
 
-printf "\n\rNew username:"
-read -p " " username < /dev/tty
+printf "\n\rNew username: "
+read username < /dev/tty
 
-useradd -m -G wheel -s /bin/bash $username
-passwd $username
+useradd -m -G wheel -s /bin/bash "$username"
+passwd "$username"
 
 sed -i 's/^#\s*\(%wheel ALL=(ALL:ALL) ALL\)/\1/' /etc/sudoers
 sed -i "/^\[user\]/,/^$/ {/^default=/d}; \$a [user]\ndefault=${username}" /etc/wsl.conf
@@ -31,11 +31,11 @@ cd ~/
 EOF
 
 printf "\n\rGenerating new ssh key..."
-printf "\n\rSet your email:"
-read -p " " useremail < /dev/tty
-[ ! -d /home/$username/.ssh ] && mkdir /home/$username/.ssh
-ssh-keygen -t ed25519 -f /home/$username/.ssh/id_ed25519 -C "$useremail"
+printf "\n\rSet your email: "
+read useremail < /dev/tty
+[ ! -d "/home/$username/.ssh" ] && mkdir "/home/$username/.ssh"
+ssh-keygen -t ed25519 -f "/home/$username/.ssh/id_ed25519" -C "$useremail"
 
-chown -R $username:$username /home/$username/.ssh
+chown -R "$username:$username" "/home/$username/.ssh"
 
-su - $username
+su - "$username"
